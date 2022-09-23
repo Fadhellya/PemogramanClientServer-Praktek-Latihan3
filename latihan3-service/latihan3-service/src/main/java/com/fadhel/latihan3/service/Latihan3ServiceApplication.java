5fc3f2c1957eba28bfc8c4a2faedef270bdf0584
Fadhellya <113502315+Fadhellya@users.noreply.github.com>
2022-09-23 17:11:05 +0700

    Add files via upload

---
+++ latihan3-service/latihan3-service/src/main/java/com/fadhel/latihan3/service/Latihan3ServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fadhel.latihan3.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Latihan3ServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Latihan3ServiceApplication.class, args);
+	}
+
+}
---
+++ latihan3-service/latihan3-service/src/test/java/com/fadhel/latihan3/service/Latihan3ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fadhel.latihan3.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Latihan3ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
